5777522aba061426ed747923d203ea93d814b6e6
seoyeon0103 <angel6462@naver.com>
2025-03-16 15:15:36 +0900

    Feat: 1475

---
+++ 202503_week02/1475/1475.java
@@ -0,0 +1,51 @@
+import com.sun.jdi.request.ExceptionRequest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static void checkingFor6or9(int[] arr, int dig){
+        if(arr[6] <= arr[9]){
+            arr[6]++;
+        }
+        else{
+            arr[9]++;
+        }
+    }
+
+    private static int maximum(int[] arr){
+        int setNum = 1;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > setNum){
+                setNum = arr[i];
+            }
+        }
+
+        return setNum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int setNum = 1;
+        int arr[] = new int[10]; //0으로 초기화
+
+        String strN = Integer.toString(N);
+
+        for (int i = 0; i < strN.length(); i++) {
+            int dig = Character.getNumericValue(strN.charAt(i));
+
+            if(dig == 6 || dig == 9){
+                checkingFor6or9(arr, dig);
+            }
+            else{
+                arr[dig]++;
+            }
+        }
+
+        setNum = maximum(arr);
+        System.out.println(setNum);
+    }
+}
