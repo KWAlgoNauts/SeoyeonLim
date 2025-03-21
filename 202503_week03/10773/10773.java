35c87922beb455ce6190587adbc5bb40db208f62
seoyeon0103 <angel6462@naver.com>
2025-03-21 12:37:04 +0900

    Feat: 10773

---
+++ 202503_week03/10773/10773.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+
+        for(int i = 0; i < K; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0 && !(stack.isEmpty())){
+                stack.pop();
+            }
+            else{
+                stack.push(num);
+            }
+        }
+
+        if(stack.size() == 0){
+            result = 0;
+        }
+
+        else{
+            for(int i = 0; i < stack.size(); i++){
+                result += stack.get(i);
+            }
+        }
+        System.out.print(result);
+    }
+}
