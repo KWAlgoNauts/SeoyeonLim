ec809feae7c7dd9193ed4274aa8961ff3ef133a8
seoyeon0103 <angel6462@naver.com>
2025-03-28 13:42:40 +0900

    Feat: 2164

---
+++ 202503_week04/2164/2164.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 0; i < N; i++){
+            queue.add(i+1);
+        }
+
+        while(queue.size() != 1){
+            int sub;
+            queue.remove();
+            sub = queue.peek();
+            queue.remove();
+            queue.add(sub);
+        }
+        System.out.print(queue.peek());
+    }
+}
