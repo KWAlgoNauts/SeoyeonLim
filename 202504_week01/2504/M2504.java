e130abe9edc4de169427aedc2fc73399f44e1965
seoyeon0103 <angel6462@naver.com>
2025-04-06 03:58:38 +0900

    Feat: 2504

---
+++ 202504_week01/2504/M2504.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class M2504{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String cmd = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        int answer = 0;
+        int temp = 1;
+        boolean error = false;
+        for(int i = 0; i < cmd.length(); i++){
+            if(cmd.charAt(i) == '('){
+                stack.push(cmd.charAt(i));
+                temp *= 2;
+            } else if (cmd.charAt(i) == '[') {
+                stack.push(cmd.charAt(i));
+                temp *= 3;
+            }
+            else{
+                // 빼는 것
+                if(cmd.charAt(i) == ')'){
+                    if(stack.isEmpty() || stack.peek() != '('){
+                        error = true;
+                        break;
+                    }
+                    if(cmd.charAt(i - 1) == '('){
+                        answer += temp;
+                    }
+                    stack.pop();
+                    temp /= 2;
+                }else if(cmd.charAt(i) == ']'){
+                    if(stack.isEmpty() || stack.peek() != '['){
+                        error = true;
+                        break;
+                    }
+                    if(cmd.charAt(i - 1) == '['){
+                        answer += temp;
+                    }
+                    stack.pop();
+                    temp /= 3;
+                }
+                else{
+                    error = true;
+                    break;
+                }
+            }
+        }
+        if(error){
+            System.out.println(0);
+        }else{
+            System.out.println(answer);
+        }
+    }
+}
